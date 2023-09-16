8c6271f89ed82310cd0b72c1bc340f7ffa2dad7f
jad kobeissi <stapler12344321@gmail.com>
2023-09-16 05:53:12 +0000

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import { initializeApp } from "firebase/app";
+import { onAuthStateChanged, getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { useEffect, useState } from "react";
+const firebaseConfig = {
+  apiKey: "AIzaSyBgqWcWDawppFxwV5Uw3SpcVNS9lbC0rJY",
+  authDomain: "app1-4ac11.firebaseapp.com",
+  projectId: "app1-4ac11",
+  storageBucket: "app1-4ac11.appspot.com",
+  messagingSenderId: "499350637297",
+  appId: "1:499350637297:web:03e100e80e136a378db750",
+  measurementId: "G-N2PNP751PC"
+};
+initializeApp(firebaseConfig)
+const auth = getAuth()
+function Login() {
+  return <button onClick={() => {
+    signInWithPopup(auth, new GoogleAuthProvider)
+  }}>login</button>
 }
-
-export default App
+function Home() {
+  const [posts, setPosts]:any = useState([])
+  const fetchPosts = async () => {
+    try {
+      const snapchot = await getDocs(collection(getFirestore(), "posts"))
+      const data = snapchot.docs.map((doc:any) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setPosts(data)
+    }catch (e) {
+      console.error(e);
+      
+    }
+  }
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  return <div className="posts">
+    {posts.map((post:any) => <div className="post" key={post.id}>
+    <div>
+    <img className="profilePicture" src={post.photo} alt="" />
+    <h2>{post.username}</h2>
+    </div>
+    <h1 className="description">{post.description}</h1>
+    <a href={post.discordProfileUrl} target="_blank"><button>Contact On Discord</button></a>
+  </div>)}
+  </div>
+}
+export default function App() {
+  type currentViewType = 'login' | 'main' | 'add'
+  const [currentView, setCurrentView] = useState<currentViewType>('login')
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurrentView("main")
+    }else {
+      setCurrentView("login")
+    }
+  })
+  return currentView === 'login' ? <Login /> : currentView === 'main' ? <Home /> : null
+}
